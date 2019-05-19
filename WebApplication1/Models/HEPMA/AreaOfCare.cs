ee72e142de1d869e42ee941f93fa1c6db46606b1
hughscouller <43971267+hughscouller@users.noreply.github.com>
2019-05-19 12:42:04 +0100

    AoCUpdate

--- WebApplication1/Models/HEPMA/AreaOfCare.cs
+++ WebApplication1/Models/HEPMA/AreaOfCare.cs
@@ -8,121 +8,67 @@ namespace WebApplication1.Models.HEPMA
 {
     public class AreaOfCare
     {
-        // meta data section /////////////////////////////////////////
+        
+
+        
         [Key]
-        public int Id { get; set; }
+        public int AoCId { get; set; }
 
-        [Display(Name = "AoC Type (IP/OP)")]
-        public string AocType { get; set; } // inpation / outpatient
-
-        [Display(Name = "AoC Name")]
-        public string Name { get; set; }
-        [Display(Name = "AoC Description")]
-        public string Description { get; set; }
-        [Display(Name = "AoC General Notes")]
-        public string Notes { get; set; }
-
-        [Display(Name = "AoC Status")]
-        public string Status { get; set; } // Pre-contact   pre-contact data gathering    Planned go-live    Actual go-live    BaU handover
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "First contact date")]
-        public DateTime? FirstContactDate { get; set; }
-        [Display(Name = "FCD Notes")]
-        public string FirstContactNotes { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Planned go-live date")]
-        public DateTime? PlannedGoLiveDate { get; set; }
-        [Display(Name = "PGD Note")]
-        public string PlannedGoLiveNotes { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Actual go-live date")]
-        public DateTime? ActualGoLiveDate { get; set; }
-        [Display(Name = "AGD Notes")]
-        public string ActualGoLiveNotes { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "BaU handover date")]
-        public DateTime? BauHandoverDate { get; set; }
-        [Display(Name = "BaU Notes")]
-        public string BauHandoverNotes { get; set; }
-
-        [Display(Name = "AoC Notes")]
-        public string AoCGeneralNotes { get; set; }
-
-        // ////////////////////////////////////////////////////////////
-        [Display(Name = "Sitel Location")]
+        // EF Navigation items section /////////////////////////////////////////
         public virtual SiteLocation SiteLocation { get; set; }
-        public int SiteLocationId { get; set; }
+        public int SiteLocationId { get; set; } 
 
-        // //////////////////////////////////////////////////////////
-        // Business process section
+        // General data section /////////////////////////////////////////
+        [Display(Name = "Name")]
+        public string AoCName { get; set; } 
+        public string AoCType { get; set; }
+        public string AocDescription { get; set; }
 
-        // use of Trak
-        [Display(Name = "ADT's on Trak")]
-        public bool ADTs { get; set; }                      // Y/N
-        [Display(Name = "In real time")]
-        public bool ADTsRealTime { get; set; }
-        [Display(Name = "ADT Notes")]
-        public string ADTsNotes { get; set; }
+        // Project data section /////////////////////////////////////////
+        public int AoCAoCImplementationOrder { get; set; }
+        public DateTime? AoCRecordOpened { get; set; }
+        public DateTime? AoCRecordClosed { get; set; }
+        public bool AoCFirstContact { get; set; }
+        public DateTime AoCFirstContactDate { get; set; }
+        public bool AoCLive { get; set; }
+        public DateTime? AoCLiveDate { get; set; }
+        public bool AoCBaU { get; set; }
+        public DateTime? AoCBaUDate { get; set; }
+        public bool AoCOnHold { get; set; }
+        public string AoConHoldReason { get; set; }
 
-        [Display(Name = "OP Clinic managed on trak")]
-        public bool OPCLinic { get; set; }                  // Y/N
-        [Display(Name = "OP managed in real time")]
-        public bool OPClinicRealTIme { get; set; }
-        [Display(Name = "OP clinic notes")]
-        public string OPClinicNotes { get; set; }
+        // Trak Use data section /////////////////////////////////////////
+        public bool AoCMedicinesReconciliation { get; set; }
+        public bool AoCMedicinesReconciliationRealTime { get; set; }
+        public bool AoCOutpatientClinicManagedOnTrak { get; set; }
+        public bool AoCOutpatientClinicManagedOnTrakInRealTime { get; set; }
+        public bool AoCADTsManagedOnTrak { get; set; }
+        public bool AoCADTsManagedOnTraIInRealTime { get; set; }
+        public bool AoCIDLsProducedInTrak { get; set; }
+        public bool AoCIDLsProducedInTrakInRealTime { get; set; }
 
-        // med rec
-        public bool MedRec { get; set; }                    // Y/N
-        public string MedRecWho { get; set; }
-        public string MedRecWhere { get; set; }
-        public string MedRecWhen { get; set; }
+        // Prescribing (recording) data section /////////////////////////////////////////
+        public bool AoCWardRounds { get; set; }
+        public bool AoCDoctorsRoom { get; set; }
+        public bool AoCNursesStation { get; set; }
+        public bool AoCOffice { get; set; }
+        public string AoCOfficeText { get; set; }
+        public bool AoCPOther { get; set; }
+        public string AoCPOtherText { get; set; }
 
-        public string MedRecNotes { get; set; }
+        // Medicines Administration (recording) data section /////////////////////////////////////////
+        public bool AoCDrugRoundAtBedside { get; set; }
+        public bool AoCDrugRoundFromCentralPoint { get; set; }
+        public bool AoCPatientComesToCentralPoint { get; set; }
+        public bool AoCMAOther { get; set; }
+        public string AoCMAOtherText { get; set; }
 
-        // Prescribing of medication
-        public bool PrescWardRounds { get; set; }           // Y/N
-        public string PrescWardRoundsNotes { get; set; }
+        // Pharmacy Checking (recording) data section /////////////////////////////////////////
+        public bool AoCBedside { get; set; }
+        public bool AoCCentralPointInWard { get; set; }
+        public bool AoCPCOher { get; set; }
+        public string AoCPCOtherText { get; set; }
 
-        public bool PrescMDTs { get; set; }                 // Y/N
-        public string PrescMDTsNotes { get; set; }
-
-        public bool PrescNursesOffice { get; set; }         // Y/N
-        public string PrescNursesOfficeNotes { get; set; }
-
-        public bool PrescOther { get; set; }                // Y/N
-        public string PrescOtherNotes { get; set; }
-
-        public bool PrescUsersEpr { get; set; }                  // Y/N
-        public string PrescUsersEprNotes { get; set; }
-
-        public string PrescGeneralNotes { get; set; }
-
-        // Administering and recording of medication
-        public bool DrugRoundBedsideRecording { get; set; }
-        public bool DrugRoundCentralPoint { get; set; }
-        public bool PatToCentralPoint { get; set; }
-        public bool Other { get; set; }
-        public string MedAdminNotes { get; set; }
-
-        public bool MedAdminUsersEpr { get; set; }                  // Y/N
-        public string MedAdminUsersEprNotes { get; set; }
-
-        // Pharmacy clinical check
-        public bool PharmCheck { get; set; }
-        public bool PharmCheckBedsite { get; set; }
-        public bool PharmCheckOther { get; set; }
-        public string PharmCheckNotes { get; set; }
-
-        public bool PharmChecUsersEpr { get; set; }                  // Y/N
-        public string PharmChecEprNotes { get; set; }
 
     }
 }
