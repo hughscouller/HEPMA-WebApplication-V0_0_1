e62f002ecc67f6e66668fdc59f6128a5b859491e
hughscouller <43971267+hughscouller@users.noreply.github.com>
2019-07-28 21:49:31 +0100

    update

--- WebApplication1/Models/AccountViewModels.cs
+++ WebApplication1/Models/AccountViewModels.cs
@@ -65,6 +65,9 @@ namespace WebApplication1.Models
     public class RegisterViewModel
     {
         [Required]
+        public string Name { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
